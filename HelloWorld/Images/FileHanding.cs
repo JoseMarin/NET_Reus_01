5aaff318194d867e09afb9754a668a3f24c64581
Jose <jmarinmalagon@gmail.com>
2020-12-22 21:43:20 +0100

    Codigo

---
+++ HelloWorld/Images/FileHanding.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Images
+
+{
+    class FileHanding
+    {
+        private const String APELLIDO = "Marín";
+        public void Saludar(String a) {
+            Console.WriteLine("Hola {0} {1}", a, APELLIDO);
+        }
+    }
+}
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace HelloWorld
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+            Images.FileHanding f1 = new Images.FileHanding();
+            f1.Saludar("Jose");
+            Console.ReadLine();
+
+            //Tipos de datos integrados
+            int x = 123;
+            Console.WriteLine(x.GetType());
+
+            int[] num = new int[5];
+            num[1] = 48;
+
+            foreach (int numb in num) {
+
+                Console.Write($"{numb} ");
+            }
+
+            Console.WriteLine(Math.Sqrt(25));
+            
+
+        }
+    }
+}
